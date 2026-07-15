6196f7639b94f304ecda103f572ab3c0c1f12838
Reginald Brixton <reginaldbrixton@gmail.com>
2026-07-15 00:22:19 +0000

    refactor: isolate analogue axes from legacy input keys

--- src/input/analogInputTypes.ts
+++ src/input/analogInputTypes.ts
@@ -1,12 +1,8 @@
-import '../engine/matchEngine';
+import type { InputState } from '../engine/matchEngine';
 
-declare module '../engine/matchEngine' {
-  interface InputState {
-    /** Horizontal analogue movement in screen space. Right is positive. */
-    moveX?: number;
-    /** Vertical analogue movement in screen space. Up is positive. */
-    moveY?: number;
-  }
-}
-
-export {};
+export type AnalogInputState = InputState & {
+  /** Horizontal analogue movement in screen space. Right is positive. */
+  moveX: number;
+  /** Vertical analogue movement in screen space. Up is positive. */
+  moveY: number;
+};
